b9b32e705cccb2e71531baacdd0414ace082ce5f
Richel <rleun005@ucr.edu>
2016-05-23 14:31:16 -0700

    Initial Push

---
+++ Release+Asserts/CS201Profiling.d
@@ -0,0 +1,272 @@
+/home/rleun/github/cs201/llvm/lib/Transforms/CS201Profiling/Release+Asserts/CS201Profiling.o \
+  /home/rleun/github/cs201/llvm/lib/Transforms/CS201Profiling/Release+Asserts/CS201Profiling.d: \
+  CS201Profiling.cpp /home/rleun/github/cs201/llvm/include/llvm/Pass.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Compiler.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Config/llvm-config.h \
+  /home/rleun/github/cs201/llvm/include/llvm/PassSupport.h \
+  /home/rleun/github/cs201/llvm/include/llvm/InitializePasses.h \
+  /home/rleun/github/cs201/llvm/include/llvm/PassInfo.h \
+  /home/rleun/github/cs201/llvm/include/llvm/PassRegistry.h \
+  /home/rleun/github/cs201/llvm/include/llvm-c/Core.h \
+  /home/rleun/github/cs201/llvm/include/llvm-c/Support.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/DataTypes.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/DenseMap.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/DenseMapInfo.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/type_traits.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/AlignOf.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/MathExtras.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/SwapByteOrder.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/SmallPtrSet.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/StringMap.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/StringRef.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Allocator.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/SmallVector.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/iterator_range.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Memory.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/CBindingWrapping.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Casting.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/RWMutex.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Threading.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Atomic.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Valgrind.h \
+  /home/rleun/github/cs201/llvm/include/llvm/PassAnalysisSupport.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Module.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Comdat.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/DataLayout.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/DerivedTypes.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Type.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/APFloat.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/APInt.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/ArrayRef.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/None.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/STLExtras.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/ErrorHandling.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Function.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Argument.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/Twine.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/ilist_node.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Attributes.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/FoldingSet.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Value.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Use.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/PointerIntPair.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/BasicBlock.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/ilist.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Instruction.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/DebugLoc.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/TrackingMDRef.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/MetadataTracking.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/PointerUnion.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/User.h \
+  /home/rleun/github/cs201/llvm/include/llvm/ADT/iterator.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Instruction.def \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/SymbolTableListTraits.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/CallingConv.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/GlobalObject.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Constant.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/GlobalValue.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/GlobalAlias.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/OperandTraits.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/GlobalVariable.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Metadata.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Metadata.def \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/CodeGen.h \
+  /home/rleun/github/cs201/llvm/include/llvm-c/TargetMachine.h \
+  /home/rleun/github/cs201/llvm/include/llvm-c/Target.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Config/Targets.def \
+  /home/rleun/github/cs201/llvm/include/llvm/Config/AsmPrinters.def \
+  /home/rleun/github/cs201/llvm/include/llvm/Config/AsmParsers.def \
+  /home/rleun/github/cs201/llvm/include/llvm/Config/Disassemblers.def \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/IRBuilder.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/ConstantFolder.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Constants.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/InstrTypes.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Instructions.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/LLVMContext.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/Options.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/CommandLine.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/Operator.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/GetElementPtrTypeIterator.h \
+  /home/rleun/github/cs201/llvm/include/llvm/IR/ValueHandle.h \
+  /home/rleun/github/cs201/llvm/include/llvm/Support/raw_ostream.h
+
+/home/rleun/github/cs201/llvm/include/llvm/Pass.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Compiler.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Config/llvm-config.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/PassSupport.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/InitializePasses.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/PassInfo.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/PassRegistry.h:
+
+/home/rleun/github/cs201/llvm/include/llvm-c/Core.h:
+
+/home/rleun/github/cs201/llvm/include/llvm-c/Support.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/DataTypes.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/DenseMap.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/type_traits.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/AlignOf.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/MathExtras.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/SmallPtrSet.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/StringMap.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/StringRef.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Allocator.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/iterator_range.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Memory.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/CBindingWrapping.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Casting.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/RWMutex.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Threading.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Atomic.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Valgrind.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Module.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Comdat.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/DataLayout.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/DerivedTypes.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Type.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/APFloat.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/APInt.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/ArrayRef.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/None.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/STLExtras.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Function.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Argument.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/Twine.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/ilist_node.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Attributes.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/FoldingSet.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Value.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Use.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/BasicBlock.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/ilist.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Instruction.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/DebugLoc.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/TrackingMDRef.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/MetadataTracking.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/PointerUnion.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/User.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/ADT/iterator.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Instruction.def:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/SymbolTableListTraits.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/CallingConv.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/GlobalObject.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Constant.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/GlobalValue.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/GlobalAlias.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/OperandTraits.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/GlobalVariable.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Metadata.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Metadata.def:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/CodeGen.h:
+
+/home/rleun/github/cs201/llvm/include/llvm-c/TargetMachine.h:
+
+/home/rleun/github/cs201/llvm/include/llvm-c/Target.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Config/Targets.def:
+
+/home/rleun/github/cs201/llvm/include/llvm/Config/AsmPrinters.def:
+
+/home/rleun/github/cs201/llvm/include/llvm/Config/AsmParsers.def:
+
+/home/rleun/github/cs201/llvm/include/llvm/Config/Disassemblers.def:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/IRBuilder.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/ConstantFolder.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Constants.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/InstrTypes.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Instructions.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/LLVMContext.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/Options.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/CommandLine.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/Operator.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/GetElementPtrTypeIterator.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/IR/ValueHandle.h:
+
+/home/rleun/github/cs201/llvm/include/llvm/Support/raw_ostream.h:
